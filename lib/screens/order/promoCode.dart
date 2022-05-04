4ba9785d5ff3e80db5b1edf7421e670eceaefc44
peter <patourtohme9@gmail.com>
2022-05-04 21:27:14 +0300

    fixed redeem code added yes and no on the button

--- lib/screens/order/promoCode.dart
+++ lib/screens/order/promoCode.dart
@@ -4,6 +4,8 @@ import 'package:provider/provider.dart';
 import 'package:yallajeye/constants/colors_textStyle.dart';
 import 'package:yallajeye/providers/order.dart';
 
+import '../../models/Adresses.dart';
+import '../../providers/address.dart';
 import '../navigation bar/navigation_bar.dart';
 
 class PromoCode extends StatefulWidget {
@@ -14,28 +16,38 @@ class PromoCode extends StatefulWidget {
 }
 
 class _PromoCodeState extends State<PromoCode> {
-
-  TextEditingController redeemController=TextEditingController();
+  TextEditingController redeemController = TextEditingController();
   final _formkey = GlobalKey<FormState>();
   bool _isloading = false;
-  String message="";
-  bool loading=false;
-  bool done=false;
+  String message = "";
+  bool loading = false;
+  bool done = false;
+  bool PlacedOrder = false;
+
   @override
   Widget build(BuildContext context) {
-    final order=Provider.of<OrderProvider>(context,listen:true);
+    final order = Provider.of<OrderProvider>(context, listen: true);
+    final address = Provider.of<AddressProvider>(context, listen: true);
+
     return WillPopScope(
       onWillPop: () {
         Navigator.of(context).pushAndRemoveUntil(
-            MaterialPageRoute(
-                builder: (context) =>
-                    Navigation()),
-                (Route<dynamic> route) => false);
+            MaterialPageRoute(builder: (context) => Navigation()),
+            (Route<dynamic> route) => false);
       },
       child: Scaffold(
         resizeToAvoidBottomInset: false,
-        appBar: AppBar(title: Text("Order Created",style: appBarText,),
-        foregroundColor: yellowColor,),
+        appBar: AppBar(
+          leading: IconButton(onPressed: (){
+            Navigator.pop(context);
+          }, icon: Icon(Icons.arrow_back)),
+          automaticallyImplyLeading: false,
+          title: Text(
+            "Order Created",
+            style: appBarText,
+          ),
+          foregroundColor: yellowColor,
+        ),
         body: SingleChildScrollView(
           child: Container(
             padding: EdgeInsets.all(25),
@@ -60,7 +72,7 @@ class _PromoCodeState extends State<PromoCode> {
                   height: 20,
                 ),
                 Form(
-                  key:_formkey,
+                  key: _formkey,
                   child: TextFormField(
                     controller: order.redeemCodeController,
                     decoration: InputDecoration(
@@ -73,7 +85,7 @@ class _PromoCodeState extends State<PromoCode> {
                             fontFamily: 'BerlinSansFB',
                             color: Color(0xFF878787)),
                         contentPadding:
-                        EdgeInsets.symmetric(vertical: 25, horizontal: 25),
+                            EdgeInsets.symmetric(vertical: 25, horizontal: 25),
                         border: OutlineInputBorder(),
                         enabledBorder: OutlineInputBorder(
                             borderRadius: BorderRadius.all(Radius.circular(15)),
@@ -86,63 +98,75 @@ class _PromoCodeState extends State<PromoCode> {
                 // SizedBox(
                 //   height: MediaQuery.of(context).size.height * 0.3,
                 // ),
-                _isloading?Center(child: Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: CircularProgressIndicator(),
-                )):!done? Center(
-                  child: Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: TextButton(
-                      onPressed: ()async{
-                        if(order.redeemCodeController.text.isEmpty) {
-                          Fluttertoast.showToast(
-                              msg: "Code cannot be empty",
-                              fontSize: 15,
-                              gravity: ToastGravity.BOTTOM,
-                              timeInSecForIosWeb: 2,
-                              textColor: Colors.white,
-                              backgroundColor: redColor,
-                              toastLength: Toast.LENGTH_SHORT
-                          );
-                        }else{setState(() {
-                          _isloading=true;
-                        });
-                        loading=  await order.redemCode();
-                        if(!loading){
-                          showDialog(context: context, builder: (context){
-                            return AlertDialog(
-                              title: Text(order.messagePlaceOrder),
-                            );
-                          });
-                        }else{
-                          setState(() {
-                            done=true;
-                          });
-                        }
-                        setState(() {
-                          _isloading=false;
-                        });}
+                _isloading
+                    ? Center(
+                        child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: CircularProgressIndicator(),
+                      ))
+                    : !done
+                        ? Center(
+                            child: Padding(
+                              padding: const EdgeInsets.all(12.0),
+                              child: TextButton(
+                                onPressed: () async {
+                                  if (order.redeemCodeController.text.isEmpty) {
+                                    Fluttertoast.showToast(
+                                        msg: "Code cannot be empty",
+                                        fontSize: 15,
+                                        gravity: ToastGravity.BOTTOM,
+                                        timeInSecForIosWeb: 2,
+                                        textColor: Colors.white,
+                                        backgroundColor: redColor,
+                                        toastLength: Toast.LENGTH_SHORT);
+                                  } else {
+                                    setState(() {
+                                      _isloading = true;
+                                    });
+                                    loading = await order.redemCode();
+                                    if (!loading) {
+                                      showDialog(
+                                          context: context,
+                                          builder: (context) {
+                                            return AlertDialog(
+                                              title:
+                                                  Text(order.messagePlaceOrder),
+                                            );
+                                          });
+                                    } else {
+                                      setState(() {
+                                        done = true;
+                                      });
 
+                                    }
+                                    setState(() {
+                                      _isloading = false;
+                                    });
+                                  }
+                                },
+                                child: const Text('Redeem',
+                                    style: TextStyle(
+                                      color: yellowColor,
+                                      fontFamily: 'BerlinSansFB',
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 15,
+                                    )),
+                                style: TextButton.styleFrom(
+                                    padding: EdgeInsets.symmetric(
+                                        vertical: 20, horizontal: 40),
+                                    side: const BorderSide(
+                                        color: Colors.black, width: 1),
+                                    shape: const RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(10))),
+                                    backgroundColor: const Color(0xFF333333)),
+                              ),
+                            ),
+                          )
+                        : Center(
+                            child: Icon(Icons.done),
+                          ),
 
-                      },
-                      child: const Text('Redeem',
-                          style: TextStyle(
-                            color: yellowColor,
-                            fontFamily: 'BerlinSansFB',
-                            fontWeight: FontWeight.bold,
-                            fontSize: 15,
-                          )),
-                      style:TextButton.styleFrom(
-                          padding: EdgeInsets.symmetric(
-                              vertical: 20, horizontal: 40),
-                          side: const BorderSide(color: Colors.black, width: 1),
-                          shape: const RoundedRectangleBorder(
-                              borderRadius:
-                              BorderRadius.all(Radius.circular(10))),
-                          backgroundColor: const Color(0xFF333333)),
-                    ),
-                  ),
-                ):Center(child: Icon(Icons.done),)
               ],
             ),
           ),
